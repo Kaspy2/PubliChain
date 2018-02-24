1daf4ba1af743171fd83fb4164c2c4c52435f435
tonkev <kevin.tonna.16@um.edu.mt>
2018-02-24 18:52:24 +0100

    hello wallet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
-import getWeb3 from './utils/getWeb3'
+import Web3 from 'web3'
+//import getWeb3 from './utils/getWeb3'
 
 class App extends Component {
-
-  render() {
-    return ( <h1> Hello, world!</h1>
-			<form>
-				<input type="text">
-			</form>
-    );
-  }
+		constructor(props){
+		super(props);
+		//if(typeof web3 !== 'undefined'){
+		//	console.log("Using web3 detected from external source like Metamask");
+		//	this.web3 = new Web3(web3.currentProvider)
+		//}else{
+			this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"))
+		//}
+		
+	}
+	
+	render() {	  
+		return (
+			<div class="dapp">
+				<h1> Hello, {this.web3.eth.coinbase}!</h1>
+				<form method="get">
+					<input type="text"></input>
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+    	);
+	}
 }
 
 export default App
