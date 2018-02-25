8b51d6ec8377950e18a079dba78ebdd21b03a8c7
Gabriel Farrugia <gfarrug@gmail.com>
2018-02-25 03:47:25 +0100

    Update of the day

--- contracts/Publication.sol
+++ contracts/Publication.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.18;
 
-import "../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol";
+// import "../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract Publication is Ownable{
+//contract Publication is Ownable{
+contract Publication {
 	// uint storedData;
 	// string name;
 
@@ -28,7 +29,7 @@ contract Publication is Ownable{
 	struct Pub{
 		uint128 pubNum;	// publication id
 		string name;	// name of publication
-		string data;	// data / publication itself
+		string data;	// data / publication itself - hash
 		uint date;		// date of publication
 	}
 
@@ -41,7 +42,9 @@ contract Publication is Ownable{
 	
 	Pub[] public publications; 				// list of publications
 	
-	function _setUsername(string _name) private onlyOwner{
+	function setUsername(string _name) public
+	// onlyOwner
+	{
 		if (userAddress[_name] != address(0)) {
 			userAddress[_name] = address(0);
 		}
@@ -50,14 +53,16 @@ contract Publication is Ownable{
 		user[msg.sender] = _name;
 	}
 
-	function _addPublication(string _name, string _data) private {
+	function addPublication(string _name, string _data) public {
 		author[uint128(publications.length)] = msg.sender;
 		publications.push(Pub(uint128(publications.length),_name, _data, now));
 		numberOfPublications[msg.sender]++;
 		PublicationAdded(_name, msg.sender);
 	}
 
-	function _getOwnPublications() private view onlyOwner returns(string[]) {
+	function _getOwnPublications() private view 
+	// onlyOwner 
+	returns(string[]) {
 		string[] memory pubNames = new string[](numberOfPublications[msg.sender]);
 		uint16 c = 0;
 		for (uint i = 0; i<publications.length; i++) {
@@ -68,27 +73,52 @@ contract Publication is Ownable{
 		}
 	}
 
-	function getPublications(string _username) public view returns(Pub[]){
-		string[] memory pubNames = new string[](numberOfPublications[msg.sender]);
+	function getPublications(string _username) public view returns(uint128[]){
+		uint128[] memory pubNums = new uint128[](numberOfPublications[msg.sender]);
 		uint16 c = 0;
 		for (uint i = 0; i<publications.length; i++) {
 			if(author[publications[i].pubNum] == userAddress[_username]) {
-				pubNames[c] = publications[i].name;
+				pubNums[c] = publications[i].pubNum;
 				c++;
 			}
 		}
+		return pubNums;
 	}
 
-	function getPublication (uint128 _publicationNumber) public view returns(Pub) {
+	function getPublication (uint128 _publicationNumber) public view returns(string,string,uint) {
 		for (uint128 i = 0; i<publications.length; i++) {
 			if(publications[i].pubNum == _publicationNumber) {
-				return publications[i];
+				return (publications[i].name, publications[i].data, publications[i].date);
 			}
 		}
 	}
 
+	function getData (uint128 _publicationNumber) public view returns(string) {
+		for (uint128 i = 0; i<publications.length; i++) {
+			if(publications[i].pubNum == _publicationNumber) {
+				return publications[i].data;
+			}
+		}
+	}
+
+	function getPs () public view returns (string) {
+		require (publications.length >= 1);
+		return publications[0].name;
+	}
 }
 
-// Publication.deployed().then(function(instance){return instance.set(69420,"IIIIIID-DYLLU");});
+// Publication.deployed().then(function(instance){return instance.addPublication("69420","IIIIIID-DYLLU");});
 // Publication.deployed().then(function(instance){return instance.getData.call();}).then(function(value){return value.toNumber()});
 // Publication.deployed().then(function(instance){return instance.getData.call();}).then(function(value){return value.toNumber()});
+
+// Publication.deployed().then(function(instance){return instance.getPublications.call();}).then(function(value){return value.toNumber()});
+
+// Publication.deployed().then(function(instance){return instance.getPublications("Dylon");}).then(function(value){return value[0].toNumber()})
+
+// Publication.deployed().then(function(instance){return instance.addPublication("NameOfPub","Data/Hash");});
+// Publication.deployed().then(function(instance){return instance.getPublications("Dylon");}).then(function(value){return value[2].toNumber()})
+
+
+// name, string (article) -> genHash() -> addPublication(name, hash)
+// pubID -> getData() -> display [no need to show hash]
+// references
